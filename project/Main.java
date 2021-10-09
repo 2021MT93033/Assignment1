d6130bff7de593973fd839a11aec5a16e623ff85
2021MT93033 <balavignesh.premkumar@gmail.com>
2021-10-09 16:18:45 +0530

    Added multiplication functionality

--- project/Main.java
+++ project/Main.java
@@ -3,8 +3,16 @@ public class Main {
 		int a = 20;
 		int b = 40;
 
+		int a1 = 31;
+		int b1 = 41;
+
 		MathFunctions mf = new MathFunctions(a, b);
 		mf.add();
 		mf.display();
+
+		MathFunctions mf1 = new MathFunctions(a1, b1);
+		mf1.multiply();
+		mf1.display();
+
 	}
 }
