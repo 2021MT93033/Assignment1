c2be7e147e77c6cdf43fffcd324031fb4b6d894b
2021MT93033 <balavignesh.premkumar@gmail.com>
2021-10-09 15:33:38 +0530

    intial setup of the project

---
+++ project/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String args[]) {
+		int a = 20;
+		int b = 40;
+
+		MathFunctions mf = new MathFunctions(a, b);
+		mf.add();
+		mf.display();
+	}
+}
---
+++ project/MathFunctions.java
@@ -0,0 +1,48 @@
+public class MathFunctions {
+	private int op1;
+	private int op2;
+	private String result;
+
+	public MathFunctions() {
+
+	}
+
+	public MathFunctions(int op1, int op2) {
+		this.op1 = op1;
+		this.op2 = op2;
+	}
+
+	public int getOp1() {
+		return op1;
+	}
+
+	public void setOp1(int op1) {
+		this.op1 = op1;
+	}
+
+	public int getOp2() {
+		return op2;
+	}
+
+	public void setOp2(int op2) {
+		this.op2 = op2;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public void add() {
+		System.out.println("the operands are op1: " + op1 + " op2: " + op2);
+		this.result = (op1 + op2) + "";
+	}
+
+	public void display() {
+		System.out.println("The value of the operation is: " + result);
+	}
+
+}
